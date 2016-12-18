db165b65e73e72ea6eb7304d78aa8db320cecd15
MIGGI <MIGGI@miggi-no-macbook-pro.local>
2016-12-18 11:10:55 +0900

    Add　：四則演算ができる電卓を作成

--- calcultor01/ViewController.swift
+++ calcultor01/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var prev_num_label: UILabel!
+    @IBOutlet weak var text_label: UILabel!
+    @IBOutlet weak var calc_label: UILabel!
+    @IBOutlet weak var ans_label: UILabel!
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //一旦各labelに空文字を入力
+        text_label.text = ""
+        prev_num_label.text = ""
+        calc_label.text = ""
+        ans_label.text = ""
+       
     }
-
+    
+    //警告を受け取ったときに呼ばれるメソッド　使用しているメモリ領域を解放する
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    //数字はすべてこのfuncで取得
+    @IBAction func num_button_pushed(_ sender: UIButton) {
+        //動的にボタンのtextを取得？
+        text_label.text! += sender.titleLabel!.text!
+    }
+    
+    //四則演算の記号は全てこちらで取得
+    @IBAction func calc_sign_pushed(_ sender: UIButton) {
+        
+        prev_num_label .text = text_label.text
+        text_label.text = ""
+        calc_label.text! = sender.titleLabel!.text!
+    }
+
+    //swift3から　AnyObject型　→Any型に変更
+    //Any型はオブジェクトであればなんでも保管可能
+    @IBAction func equal_pushed(_ sender: Any) {
+        if Int(prev_num_label.text!) != nil && Int(text_label.text!) != nil {   //両方になにがしかの文字列が入っていれば四則演算可能
+            //四則演算の分岐
+            if calc_label.text == "+" {
+                ans_label.text = String( Int(prev_num_label.text!)! + Int(text_label.text!)!)
+            }
+            
+            if calc_label.text == "-" {
+                ans_label.text = String( Int(prev_num_label.text!)! - Int(text_label.text!)!)
+            }
+
+            if calc_label.text == "×" {
+                ans_label.text = String( Int(prev_num_label.text!)! * Int(text_label.text!)!)
+            }
+
+            if ans_label.text == "÷" {
+                text_label.text = String( Int(prev_num_label.text!)! / Int(text_label.text!)!)
+            }
+
+        }
+    }
+    
+    //ACでオールクリア
+    @IBAction func allclear_pushed(_ sender: UIButton) {
+        text_label.text = ""
+        prev_num_label.text = ""
+        calc_label.text = ""
+        ans_label.text = ""
+
+    }
+    
+    
+    
 
 }
 
